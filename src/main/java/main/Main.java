b4d82e98d57c2d5cc769c944d215788f20bd2daa
Vanja Kovinic <vanjakovinic009@gmail.com>
2022-11-14 23:46:56 +0100

    SREDJENO

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,14 +1,9 @@
 package main;
 
 
-import Data.MyException;
-import paket.Configuration;
+
 import paket.FileManager;
 import paket.RepoManager;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 public class Main {
@@ -16,12 +11,15 @@ public class Main {
     private static String local = "filelocal.FileLocalImpl";
     private static String drive = "paket.GoogleDriveImpl";
 
-    public static void main(String[] args) throws ClassNotFoundException, MyException {
-
-        Class.forName(drive);
-        FileManager fileLocal = RepoManager.getRepo();
-        fileLocal.createRoot("", "probaRoot");
-        fileLocal.mkdir("", "dir",7, false);
+    public static void main(String[] args){
+        try {
+            Class.forName(drive);
+            FileManager fileLocal = RepoManager.getRepo();
+            fileLocal.createRoot("C:\\Users\\Vanja\\Desktop", "probaRoot");
+            fileLocal.mkdir("", "dir",7, false);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
