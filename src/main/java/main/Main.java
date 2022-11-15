942974ff7e370379ce391a23dc040a03b63e3397
Vanja Kovinic <vanjakovinic009@gmail.com>
2022-11-15 02:30:56 +0100

    Postavljene osnove

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,8 @@ package main;
 import paket.FileManager;
 import paket.RepoManager;
 
+import java.util.Scanner;
+
 
 public class Main {
 
@@ -12,14 +14,30 @@ public class Main {
     private static String drive = "paket.GoogleDriveImpl";
 
     public static void main(String[] args){
+        System.out.println("1. Local file system\n2. Google Drive");
+        Scanner sc = new Scanner(System.in);
+        int opt= 5;
+        String fileSystem = "";
+        while(opt != 1 && opt != 2){
+            System.out.println("Please enter 1 or 2!");
+            opt = isNumeric(sc.nextLine());
+        }
+        if(opt == 1)
+            fileSystem = local;
+        else fileSystem = drive;
         try {
-            Class.forName(drive);
-            FileManager fileLocal = RepoManager.getRepo();
-            fileLocal.createRoot("C:\\Users\\Vanja\\Desktop", "probaRoot");
-            fileLocal.mkdir("", "dir",7, false);
+            Class.forName(fileSystem);
+
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
 
+    private static int isNumeric(String str){
+        try {
+            return Integer.parseInt(str);
+        }catch (Exception e){
+            return -1;
+        }
     }
 }
